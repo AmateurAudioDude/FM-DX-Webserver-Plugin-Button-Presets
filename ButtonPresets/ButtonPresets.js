7abe0785624bc7e39793bfc31ac43df8eac1c942
AmateurAudioDude <168192910+AmateurAudioDude@users.noreply.github.com>
2024-09-13 21:13:08 +1000

    v1.2.2: Changed show all presets to optional menu

---
+++ ButtonPresets/ButtonPresets.js
@@ -0,0 +1,14 @@
+// Plugin configuration, this is used in the administration when plugins are loaded
+var pluginConfig = {
+    name: 'Button Presets',
+    version: '1.2.2',
+    author: 'AAD',
+    frontEndPath: 'ButtonPresets/pluginButtonPresets.js'
+}
+
+// Backend (server) changes can go here...
+
+// Don't change anything below here if you are making your own plugin
+module.exports = {
+    pluginConfig
+}
